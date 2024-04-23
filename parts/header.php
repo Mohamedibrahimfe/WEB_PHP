79413c8ea98cab0b811de5833a9d949251d381ed
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-04-24 01:43:03 +0200

    little changes on header

--- parts/header.php
+++ parts/header.php
@@ -2,7 +2,7 @@
     <link rel="stylesheet" href="parts/header.css">
 </head>
 <nav class="navbar">
-    <a class="nav-logo" href="/index.html">Kemet Chronicles
+    <a class="nav-logo" href="home.php">Kemet Chronicles
     </a>
     <div class="nav-toggle" id="mobile-menu">
         <span class="bar"></span>
@@ -10,17 +10,18 @@
         <span class="bar"></span>
     </div>
     <div class="navbar-menu">
-        <li><a class="nav-link" href="/index.html">Home</a></li>
+        <li><a class="nav-link" href="#hero">Home</a></li>
         <li class="nav-item ">
-            <a href="#" class="dropdown-toggle nav-link">Articles</a>
+            <a href="#articles" class="dropdown-toggle nav-link">Articles</a>
             <ul class="dropdown" id="dropdown">
                 <a href="#" class="child"> Nobel prize</a>
                 <a href="#" class="child"> Literature</a>
             </ul>
         </li>
-        <li><a class="nav-link" href="/about.html">About</a></li>
         <li><a class="nav-link" href="/contact.html">Skills</a></li>
-        <li><a class="nav-link" href="/contact.html">Our Team</a></li>
-        <li><a class="nav-link" href="reg/signin.php">Log Out</a></li>
+        <li><a class="nav-link" href="#about_us">About</a></li>
+        <li><a class="nav-link" href="#team_members">Our Team</a></li>
+        <li><a class="nav-link" href="#contact_us">Contact</a></li>
+        <!-- <li><a class="nav-link" href="reg/signin.php">Log Out</a></li> -->
     </div>
 </nav>
